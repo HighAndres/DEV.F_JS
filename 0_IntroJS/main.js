d38c2cedcdd224f6690db03aee48f841319c6c72
HighAndres <andcls98@gmail.com>
2022-06-06 17:30:02 -0500

    0_IntroJS

---
+++ 0_IntroJS/main.js
@@ -0,0 +1,18 @@
+//Esto es una alerta
+alert('Hola Mundo');
+
+//Envair msj a la consola
+console.log('Mensaje en consola')
+console.log('otro mensaje');
+
+//Errores en consola
+
+console.error('Msj error consola');
+
+//Mostrar un warning
+
+console.warn('Esto es un warning');
+
+//Entrada
+
+prompt('Intruduce tu  nombre');
