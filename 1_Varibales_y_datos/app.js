902862d2cb8282e956a0cd61a5d50e58c0f6e2fb
HighAndres <andcls98@gmail.com>
2022-06-08 15:37:20 -0500

    v1

---
+++ 1_Varibales_y_datos/app.js
@@ -0,0 +1,59 @@
+//Creacion y asignacion de una variable//
+var nombre = 'Andres Celis';
+
+//String
+
+var pais = 'Mexico';
+var fraseSecreta = 'Nombre de tu mascota';
+var letra = 'a';
+var idioma = 'español';
+ 
+
+var info = nombre + pais;
+console.log(info);
+//La variables se pueden reasignar
+
+pais = 'Alemania';
+letra = 'x';
+edad = 10;
+
+//Mostrar en consola 
+console.log(fraseSecreta);
+console.log(nombre);
+
+//number
+
+var edad = 20;
+
+var telefono = 12345678;
+
+var resultado = -50;
+
+var promedio = 97.7;
+
+
+//concatenar --> unir dos o mas strings o dos o mas valores
+
+var infoNueva = nombre + pais;
+console.log(infoNueva);
+
+var infomas = nombre + idioma + fraseSecreta;
+console.log(infomas);
+
+var infocon = pais + nombre + '' +  ' ,hola';
+console.log(infocon);
+
+console.log('Mi nombre es ' + nombre + ' y mi promedio es '+ promedio);  //cuando se concatena un numero con un string se vuelve string
+
+//Boolean
+
+var esAdulto = true;
+var completado = false;
+var graduado = false;
+var cumple = true;
+var Mexicano = false;
+
+//Reasignacion de valores no diferencia tipo de datos
+
+
+
