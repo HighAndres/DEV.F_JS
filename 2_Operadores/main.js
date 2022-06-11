01cc9ced3d71164d8f180aebabbbb54fa64968ce
HighAndres <andcls98@gmail.com>
2022-06-11 14:00:01 -0500

    c2

---
+++ 2_Operadores/main.js
@@ -0,0 +1,19 @@
+
+
+
+//console.log('Operadores');
+
+//Operadores matematicos
+//Concatenar
+//console.log('Esto es'+ " "+ 'un ejemplo');
+//console.log('Esto es', 'un ejemplo');
+
+//para cambiar el tipo de vairale
+
+var noEsunNumero = '10';
+//tipo string
+console.log(typeof noEsunNumero);
+
+console.log(+noEsunNumero);
+// tipo numero
+console.log(typeof noEsunNumero);
