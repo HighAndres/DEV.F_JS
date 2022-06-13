559223c65c3b463fc71526304f6d515b0999d974
HighAndres <andcls98@gmail.com>
2022-06-13 14:07:46 -0500

    ejercicio2

---
+++ 2_Operadores/ejercicioCondicionales.js
@@ -0,0 +1,9 @@
+console.log('condicionales');
+
+let edad = prompt('Ingresa tu edad');
+let genero = prompt('Cual es tu genero  H O M');
+
+
+if(genero === 'M' ){
+    let embarazo = prompt('Estas embarazda');
+}
--- 2_Operadores/main.js
+++ 2_Operadores/main.js
@@ -1,6 +1,6 @@
 
 
-
+//let y var el primero manda un warning cuando esta duplicado y no deja volver a declarar una variable existente 
 //console.log('Operadores');
 
 //Operadores matematicos
@@ -10,10 +10,86 @@
 
 //para cambiar el tipo de vairale
 
-var noEsunNumero = '10';
+//var noEsunNumero = '10';
 //tipo string
-console.log(typeof noEsunNumero);
+//onsole.log(typeof noEsunNumero);
 
-console.log(+noEsunNumero);
+//console.log(+noEsunNumero);
 // tipo numero
-console.log(typeof noEsunNumero);
+//console.log(typeof noEsunNumero);
+
+
+//let numero1 = 10;
+//let numero2 = 9;
+
+//console.log(numero1 >= numero2);
+
+//operador de asignacion =
+//let numero 
+
+//let numero1 = 10; // tipo numero 
+//let numero2 = '10';// tipo string
+
+//operador igual que ==
+//nuevo javasript usamos triple igual
+//console.log(numero1  == numero2); // con 2 == nos sale true en la consola porque no toma en cuenta el tipo de variable en diferencia si usarmos  3 =
+//console.log(numero1 === numero2);
+
+//operador de distinto que !==
+//console.log(numero1 !== numero2);
+
+//la variable esta declararda peor no tiene ningun valor 
+//let saludo;
+//console.log(saludo);
+
+//null se asigna a una variable que intencionalmente se le agrega este valor
+//let mensajes = null;
+//console.log(mensajes);
+
+//operador de asiganacion matematica
+//+=, -=, *=, dependiendo el signo aplica la operacion y a;ade el nuevo valor a la variable
+//let numero = 10;
+//console.log(numero += 2);
+//console.log(`el nuevo numero es ${numero}`); //no son comillas simples es acento grave 
+
+//evalua si el valor de una variable es true o false en este caso solo mandara true cuando las dos variables sean true si alguna de estas cambia marcara false 
+//operador and &&
+//let mensajes = true;
+//let noticias = true;
+//let memes = false;
+
+//console.log(mensajes && noticias);
+
+//operador OR ||
+//retorna un true si alguna de las dos condiciones es verdader
+//console.log(mensajes || noticias || memes);
+//let mensajes = 0 // si el valor de let es o o null esta nos mostrara no hay mensajes
+//let numeroDeMensajes = mensajes || 'no hay mensajes';
+
+//console.log(numeroDeMensajes);
+
+//bloque IF
+//if(10 > 5){
+//    console.log('si es mayor a 5'); //Esto solo se mostrara en consola si la condicion se cumple
+//}
+
+//ELSE
+//if(10 > 58){ //si la condicon  no se cumple se ejecuta lo del else
+ //   console.log('es verdadero');
+//} else {
+//    console.log('es falso');
+//}
+
+//ELSE IF
+//let numero1 = 10;
+//let numero2 = 3;
+//let numero3 = 8;
+
+//if(numer1 < numero2){
+ //   console.log(`el numero ${numero1} en menor al  ${numero2} `);
+//}else if(numero2 === numero3){
+ //   console.log(`el numero ${numero2} en igual al  ${numero3} `);
+//}else{
+   // console.log('nada es verdad');
+//}
+
