a8166aa115237800ce45f36983a41a9d0b7b6f1c
HighAndres <andcls98@gmail.com>
2022-06-14 17:21:13 -0500

    c2

--- 2_Operadores/ejercicioCondicionales.js
+++ 2_Operadores/ejercicioCondicionales.js
@@ -1,9 +1,34 @@
-console.log('condicionales');
-
-let edad = prompt('Ingresa tu edad');
-let genero = prompt('Cual es tu genero  H O M');
+/*console.log('Ejercicio condicionales');
 
 
-if(genero === 'M' ){
-    let embarazo = prompt('Estas embarazda');
+//Se declaran variables
+let edad = prompt('Ingresa tu edad'); //se guarda como string con un prompt
+edad = Number(edad); //esto es para convertir el numero a string
+let genero = '';
+let embarazo = '';
+let ubicacion = '';
+
+if(edad >= 18){
+   ubicacion = prompt('Vives en un municipio fronterizo')
+   genero = prompt('Cual es tu genero? H/M')
 }
+
+
+if(genero === 'M' && edad >= 18 ){
+    let embarazo = prompt('Estas embarazada?')
+}
+
+if(edad >= 30 && !embarazo){
+    console.log('tienes mas de 30 si puedes vacunarte')
+}else if (edad >= 18 && genero === 'M' && embarazo === 'si'){
+    console.log('Eres mujer y estas embarazada puedes vacunarte')
+}else if(edad >= 18 && ubicacion === 'si' ){
+    console.log('estas en la frontera si puedes vacunarte')
+}else {
+    console.log('no hay vacunas para ti')
+}
+
+*/
+
+
+
