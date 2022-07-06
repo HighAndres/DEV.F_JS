0b2e11d56a79a00ba50fdbd7ecb6c3622f469835
HighAndres <andcls98@gmail.com>
2022-07-06 17:58:52 -0500

    JS

--- Ejercicios/ejercicios.js
+++ Ejercicios/ejercicios.js
@@ -1,35 +1,117 @@
+
+//<h3>1</h3>
+
+//Paco hizo travesuras en el salón y la maestra lo castigó, para poder salir tiene escribir en el pizarrón todos los números pares  del 1 al 100, ¿puedes ayudarle a Paco a hacer un algoritmo que haga esto por el?
+
 /*
-<h3>1</h3>
+ function imprimirNumeros(){
+   for (let i = 2; i <= 100; i+=2)
+     console.log(i)
+ }
 
-Paco hizo travesuras en el salón y la maestra lo castigó, para poder salir tiene escribir en el pizarrón todos los números pares  del 1 al 100, ¿puedes ayudarle a Paco a hacer un algoritmo que haga esto por el?
+ imprimirNumeros()
+ 
+let numeroPar = 1;
 
-<h3>2</h3>
-Escribir todos los números que terminan en 0 y estén entre 2 y 121
+for(i = 0; i <= 100; i++){
+//usar siempre el triple igual
+if (numeroPar % 2 === 0){
+     console.log(numeroPar)
+ }
+   numeroPar++
+}
+*/
 
-<h3>3</h3>
-<h4>Extra</h4>
-Joel lleva 5 materias, el quiere saber su promedio al recibir sus calificaciones,
+//<h3>2</h3>
+//Escribir todos los números que terminan en 0 y estén entre 2 y 121
+
+//<h3>3</h3>
+//<h4>Extra</h4>
+//Joel lleva 5 materias, el quiere saber su promedio al recibir sus calificaciones,
 // ¿podrías ayudarle a crear un algoritmo que haga eso por el?
+/*
+let calificaciones = [10, 8 , 9, 7, 7];
+
+let sumaCalificaciones = 0;  
+//AQUI SE SUMA Y SIN EL LENGTH NO FUNCIONA PORQUE "i" DEBE DE SER MENOR AL LARGO DEL ARREGLO 
+for(let i = 0; i < calificaciones.length; i ++){
+    sumaCalificaciones = sumaCalificaciones + calificaciones[i]
+}
+ console.log(sumaCalificaciones);
 
 
-### Ejercicios de Funciones
-<h3>1</h3>
- Hacer una función que convierta pesos a dólares
+ console.log(`las calificacion es: ${sumaCalificaciones/calificaciones.length}`)
 
- let pesos = 400;
-
- func
+*/
 
 
+//### Ejercicios de Funciones
+//<h3>1</h3>
+ //Hacer una función que convierta pesos a dólares
+/*
+let pesos = 400;
+
+function cambio(a){
+    let dolar = 19.59;
+    let convert = a/dolar;
+    return convert;
+}
+
+console.log(cambio(pesos));
+ 
+*/
+
+
+/*
+
+//<h3>2</h3>
+//Hacer una función que cuente el número de caracteres de una palabra
+
+function contador(palabra){
+    if (typeof palabra !== "string"){
+        return "Tiene que ser una palabra"
+    }
+    let numeroPalabras = palabra.length
+    return numeroPalabras
+}
+console.log(`El numero de caracteres  es ${contador("AndresCelis")} ` );
+
+
+*/
+
+ 
+//<h3>3</h3>
+//Hacer una función que halle el máximo entre dos números
+/*
+function maximo2Num(a,b){
+    let respuesta
+    if(a < b){
+        respuesta = a + 'Es menor que' + b;
+        return respuesta;
+    }else if(a > b){
+        respuesta = a + 'Es mayor que' + b;
+        return respuesta
+    }else if (a === b){
+        respuesta = a+ 'Es igual que' + b;
+        return respuesta;
+    }
+}
+
+console.log(maximo2Num('8','7'));
+ */
 
 
 
-<h3>2</h3>
- Hacer una función que cuente el número de caracteres de una palabra
+ //Hacer una función que halle el máximo entre tres números
+let numeros = [3,4,5,6];
 
-<h3>3</h3>
- Hacer una función que halle el máximo entre dos números
- Hacer una función que halle el máximo entre tres números
+Math.max(...numeros);
+console.log(numeros);
+
+
+
+/*
+
  Hacer una función que calcule el IVA de una cantidad (16%)
 
 <h3>4</h3>
